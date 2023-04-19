6c972ba7b899e1a723928c2860e746587d538cb0
djtanner <zshariat08@gmail.com>
2023-04-19 23:21:19 +0400

    updated start screen design and background

--- states/StartState.lua
+++ states/StartState.lua
@@ -2,6 +2,15 @@ StartState = Class{__includes = BaseState}
 
 function StartState:enter()
     self.level = 1
+
+    backgrounds = {
+        { image = love.graphics.newImage("graphics/Clouds6/1.png"), speed = 10, x=0 },
+        { image = love.graphics.newImage("graphics/Clouds6/2.png"), speed = 20, x=0 },
+        { image = love.graphics.newImage("graphics/Clouds6/3.png"), speed = 30, x=0 },
+        { image = love.graphics.newImage("graphics/Clouds6/4.png"), speed = 40, x=0 },
+        { image = love.graphics.newImage("graphics/Clouds6/5.png"), speed = 50, x=0 }, 
+        { image = love.graphics.newImage("graphics/Clouds6/6.png"), speed = 60, x=0 }, 
+    }
 end
 
 function StartState:update(dt)
@@ -17,14 +26,16 @@ function StartState:update(dt)
             love.event.quit()
         end
     end
-
+    updateBackgrounds(dt)
 
 end
 
 function StartState:render()
     -- title
-    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    --love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
+    drawBackgrounds()
+    love.graphics.setColor(10/255, 77/255, 104/255, 255/255)
     love.graphics.printf("SPACE INVADERS", 0, VIRTUAL_HEIGHT / 3,
         VIRTUAL_WIDTH, 'center')
 
