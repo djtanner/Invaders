2bfc567902e86228d6b83746e13dd383b9afdbaf
djtanner <zshariat08@gmail.com>
2023-04-19 07:37:49 +0400

    render score and level on gameover screen

---
+++ states/GameOverState.lua
@@ -0,0 +1,42 @@
+GameOverState = Class{__includes = BaseState}
+
+function GameOverState:enter(params)
+    self.level = params.level
+    self.score = params.score
+end
+
+function GameOverState:update(dt)
+    
+
+    function love.keypressed(key)
+    if key == enter or key =='return' then
+        gStateMachine:change('play', {
+            score = 0, level = 1
+        })
+    end
+
+        if key == 'escape' then
+            love.event.quit()
+        end
+    end
+
+
+end
+
+function GameOverState:render()
+    -- title
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    love.graphics.printf("GAME OVER", 0, VIRTUAL_HEIGHT / 3,
+        VIRTUAL_WIDTH, 'center')
+
+
+        love.graphics.printf("PRESS ENTER TO PLAY AGAIN", 0, VIRTUAL_HEIGHT / 2,
+        VIRTUAL_WIDTH, 'center')
+
+        
+
+        renderScore(self.score)
+        renderLevel(self.level)
+
+end
